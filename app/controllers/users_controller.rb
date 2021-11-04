5f4895d78d0a607e98c616707655b9e387e889dc
Scott Donnan <scott.donnan@me.com>
2021-11-04 09:07:30 -0500

    commit tree 0939393072c9ef8f01b9a131513fb45d683f6055

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+
+    def show
+        user = User.find(params[:id])
+        if user.valid?
+            render json: user, include: :items
+        else 
+            render json: {errors: user.errors}, status: :not_found
+        end
+    end
 end
